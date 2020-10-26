bdeec48ce48e72c1742ccfc750f946238d5f721f
Evan Steinberg <es7012a@american.edu>
2020-10-26 11:17:06 -0400

    grouped data by month for each state

--- data-raw/clean_covid_tracking.R
+++ data-raw/clean_covid_tracking.R
@@ -7,3 +7,15 @@ ctracking <- read_csv("data-raw/all-states-history (2).csv")
 # remove January data
 ctracking <- ctracking %>%
   filter(date >= "2020-02-01")
+
+#remove unnecessary data
+ctracking <- ctracking %>%
+  select(-dataQualityGrade)
+
+# group data by month
+ctracking <- ctracking %>%
+  mutate(Month = month(ymd(date))) %>%
+  select(Month, everything()) %>%
+  select(-date) %>%
+  group_by(Month, state) %>%
+  summarise_all(sum)
