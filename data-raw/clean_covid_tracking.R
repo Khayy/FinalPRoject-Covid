784b700ff791ed15c02861b9cf90f84d6c9a915d
Evan Steinberg <es7012a@american.edu>
2020-10-26 10:50:34 -0400

    removed January data

--- data-raw/clean_covid_tracking.R
+++ data-raw/clean_covid_tracking.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+library(lubridate)
 
 
 ctracking <- read_csv("data-raw/all-states-history (2).csv")
+
+# remove January data
+ctracking <- ctracking %>%
+  filter(date >= "2020-02-01")
