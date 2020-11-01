b1f52d75ec9385bc7b2de5af40a05683fcee2e46
Evan Steinberg <es7012a@american.edu>
2020-11-01 14:37:17 -0500

    finished cleaning political parties

--- data-raw/clean_political_parties.R
+++ data-raw/clean_political_parties.R
@@ -3,9 +3,9 @@ library(tidyverse)
 # read in party data 
 parties <- read_csv("data-raw/state_parties.csv")
 
-# rename Party of Govonor column
+# rename Party of Governor column
 parties <- parties %>%
-  rename(Party = `Party of Govoner`)
+  rename(Party = `Party of Govoner`, Name = State, state = Abbreviation)
 
 # Export to rds file
 write_rds(parties, path = "./data/political_parties_of_states.rds")
