74d8623975cb80eacf8b69d37e0d4da4b5a82d95
Evan Steinberg <es7012a@american.edu>
2020-11-02 23:28:01 -0500

    have a base plot working. Now owrking to create correct plots as needed

--- R/boxplots.R
+++ R/boxplots.R
@@ -12,34 +12,72 @@ bplot <- left_join(ctracking, death, by = c("state" = "abb"))
 
 #Select the important Variables
 bplot <- bplot %>%
-    select(state, NAME, `Age Group`, `Condition Group`, Condition, 
+    select(state, NAME, `Age Group`, Condition, 
            `Number of COVID-19 Deaths`, positive, onVentilatorCurrently) %>%
     rename(Positive_Test = positive,
            On_Ventilator = onVentilatorCurrently,
            Age_Group = `Age Group`,
-           Condition_Group = `Condition Group`,
-           Number_of_Deaths = `Number of COVID-19 Deaths`)
+           Number_of_Deaths = `Number of COVID-19 Deaths`,
+           Name = NAME)
 
+#Get rid of rows whose NAME = NA
+bplot <- bplot %>%
+    filter(is.na(Name) != "TRUE")
 
 ui <- fluidPage(
   
-    sidebarLayout(
-        sidebarPanel(
+    fluidRow(title = "Inputs",
+        column(4,
             varSelectInput("boxvar1", "Please a Variable", data = bplot,
                            selected = "state"),
             varSelectInput("boxvar2", "Please another Variable", data = bplot,
                            selected = "Number_of_Deaths")
-        ), # End sidebarPanel
-        
-        mainPanel(
-            
+        ), # End column
+        column(8)
+    ), # end fluidRow
+    fluidRow(title = "Outputs",
+        column(12,
+            plotOutput("boxplot")
         ) # end mainPanel
-    ) # end sidebarLayout
+    ) # end fluidRow
     
 )
 
 server <- function(input, output, session) {
   
+    output$boxplot <- renderPlot({
+        validate(need(input$boxvar1 != "Month" & input$boxvar2 != "Month", 
+                      "Please select a different variable"))
+        
+        a <- bplot %>%
+            select(input$boxvar1, input$boxvar2)
+        
+            if (input$boxvar1 == "state") {
+                a <- a %>%
+                    group_by(state) %>%
+                    summarize(sum(!!input$boxvar2, na.rm = T)) 
+                names(a)[2] <- "b"
+               p1 <- ggplot(a, mapping = aes(x = reorder(!!input$boxvar1, -b), 
+                                        y = b)) +
+                    geom_col()
+            }
+            else if (input$boxvar2 == "state") {
+                #FIX THIS
+                a <- a %>%
+                    group_by(state) %>%
+                    summarize(sum(!!input$boxvar1, na.rm = T)) 
+                p1 <- ggplot(a, mapping = aes(x = sum(!!input$boxvar1, na.rm = T),
+                                              y = !!input$boxvar2)) +
+                    geom_boxplot()
+            }
+        
+            p1 +
+                theme_bw() +
+                theme(axis.text.x = element_text(angle = 75, vjust = 0.5, hjust=1))
+            
+            p1
+    })
+    
 }
 
 shinyApp(ui, server)
