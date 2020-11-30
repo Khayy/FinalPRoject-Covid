4b49fa207d31f54412f1eebfed99ae963fe04c6f
Evan Steinberg <es7012a@american.edu>
2020-11-30 13:03:42 -0500

    added per 100,000 to mapping

--- R/evan_extra_mapping.R
+++ R/evan_extra_mapping.R
@@ -13,6 +13,7 @@ library(tigris)
 # Load Data
 parties <- read_rds("../data/political_parties_of_states.rds")
 ctracking <- read_rds("../data/ctracking.rds")
+pop <- read_rds("../data/pop.rds")
 
 # combine data sets
 ctracking2 <- left_join(ctracking, parties, by = "state")
@@ -44,6 +45,23 @@ ctracking2 <- ctracking2 %>%
 
 # Get per capita
 
+ctracking2 <- left_join(ctracking2, pop, by = c("NAME" = "NAME"))
+ctracking2 <- ctracking2 %>%
+  rename(Population = POPESTIMATE2019)
+
+ctracking2 <- ctracking2 %>%
+  mutate(deaths_per_capita = death/Population * 100000,
+         positive_test_per_capita = Positive_Test/Population * 100000,
+         negative_test_per_capita = Negative_Test/Population * 100000) 
+
+ctracking2 <- ctracking2 %>%
+  mutate_at(vars(deaths_per_capita, positive_test_per_capita, negative_test_per_capita),
+            funs(round(., digits = 5)))
+
+# make dataset for variable list
+ctracking4 <- ctracking2 %>%
+  select(-positive_test_per_capita, -deaths_per_capita, 
+         -negative_test_per_capita, - Population)
 
 #get the state spacial data
 states <- states()
@@ -61,7 +79,7 @@ ui <- fluidPage(
 
     sidebarLayout(
         sidebarPanel(
-            varSelectInput("mapvar", "Please Select a Variable to Map", data = ctracking2,
+            varSelectInput("mapvar", "Please Select a Variable to Map", data = ctracking4,
                            selected = "death"),
             checkboxInput("relative", "map unit per 100,000", value = FALSE),
             textOutput("intro"),
@@ -88,7 +106,120 @@ server <- function(input, output, session) {
       setView(-98.483330, 38.712046, zoom = 4)
     
     if (input$relative == TRUE) {
-      
+      if (input$mapvar == "death") {
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$deaths_per_capita)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$deaths_per_capita)
+        
+        leaf %>%
+          addPolygons(data = mapping, 
+                      fillColor = ~pal(mapping$deaths_per_capita), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$deaths_per_capita, 
+                    position = "bottomright")
+        
+      } else if (input$mapvar == "Hospitalized") {
+        
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$Hospitalized)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$Hospitalized)
+        
+        leaf %>%
+          addPolygons(data = mapping , 
+                      fillColor = ~pal(mapping$Hospitalized), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$Hospitalized, 
+                    position = "bottomright")
+        
+      } else if (input$mapvar == "On_Ventilator") {
+        
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$On_Ventilator)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$On_Ventilator)
+        
+        leaf %>%
+          addPolygons(data = mapping , 
+                      fillColor = ~pal(mapping$On_Ventilator), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$On_Ventilator, 
+                    position = "bottomright")
+        
+      } else if (input$mapvar == "Negative_Test") {
+        
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$negative_test_per_capita)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$negative_test_per_capita)
+        
+        leaf %>%
+          addPolygons(data = mapping , 
+                      fillColor = ~pal(mapping$negative_test_per_capita), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$negative_test_per_capita, 
+                    position = "bottomright")
+        
+      } else if (input$mapvar == "Positive_Test") {
+        
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$positive_test_per_capita)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$positive_test_per_capita)
+        
+        leaf %>%
+          addPolygons(data = mapping , 
+                      fillColor = ~pal(mapping$positive_test_per_capita), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$positive_test_per_capita, 
+                    position = "bottomright")
+        
+      } else if (input$mapvar == "Recovered") {
+        
+        # Color palette
+        pal <- colorNumeric("Reds", domain=mapping$Recovered)
+        
+        # Setting up the pop up text
+        popup_sb <- paste0(mapping$NAME,": ", mapping$Recovered)
+        
+        leaf %>%
+          addPolygons(data = mapping , 
+                      fillColor = ~pal(mapping$Recovered), 
+                      fillOpacity = 0.7, 
+                      weight = 0.2, 
+                      smoothFactor = 0.2, 
+                      popup = ~popup_sb) %>%
+          addLegend(pal = pal, 
+                    values = mapping$Recovered, 
+                    position = "bottomright")
+        
+      }
     }
     else {
       if (input$mapvar == "death") {
@@ -214,10 +345,32 @@ server <- function(input, output, session) {
       is.numeric(ctracking2[[input$mapvar]]) == "TRUE" & input$mapvar != "Month",
       "Please select a numeric variable"
     ))
+    
+    if (input$relative == TRUE) {
+      if (input$mapvar == "death") {
+        t.test(ctracking2$deaths_per_capita ~ Party, data = ctracking2) %>%
+          tidy() %>%
+          select(`P-value` = p.value, estimate,
+                 `95% Lower` = conf.low, `95% Upper` = conf.high)
+      } else if (input$mapvar == "Positive_Test") {
+        t.test(ctracking2$positive_test_per_capita ~ Party, data = ctracking2) %>%
+          tidy() %>%
+          select(`P-value` = p.value, estimate,
+                 `95% Lower` = conf.low, `95% Upper` = conf.high)
+      } else if (input$mapvar == "Negative_Test") {
+        t.test(ctracking2$negative_test_per_capita ~ Party, data = ctracking2) %>%
+          tidy() %>%
+          select(`P-value` = p.value, estimate,
+                 `95% Lower` = conf.low, `95% Upper` = conf.high)
+      }
+    }
+    else {
       t.test(ctracking2[[input$mapvar]] ~ Party, data = ctracking2) %>%
         tidy() %>%
         select(`P-value` = p.value, estimate,
                `95% Lower` = conf.low, `95% Upper` = conf.high)
+    }
+      
   }) # End renderTable
   
   output$warning <- renderText({
