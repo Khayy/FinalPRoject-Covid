7c011da29a8225b3b6a21c26a88eef35063ab7da
Evan Steinberg <es7012a@american.edu>
2020-12-04 18:31:14 -0500

    baseline

--- R/ANOVA.R
+++ R/ANOVA.R
@@ -9,11 +9,11 @@ age_gender <- read_rds("../data/age_gender.rds")
 age_gender%>%
   mutate("Not Applicable" = " ") -> age_gender
 
-COVID_Deaths%>%
+death%>%
   rename(State = NAME)%>%
   rename(Deaths = "Number of COVID-19 Deaths")%>%
   mutate("Not Applicable" = " ")%>%
-  select(State, "Condition Group", "Condition", "Age Group", Deaths, "Not Applicable")-> COVID_Deaths
+  select(State, "Condition Group", "Condition", "Age Group", Deaths, "Not Applicable")-> death
 
 
 
@@ -41,10 +41,10 @@ ui <- fluidPage(
                                 sidebarLayout(sidebarPanel(
                                   radioButtons("type_2","Would you like to conduct a one-way or two-way ANOVA?", choices = c("One-way", "Two-way")),
                                   varSelectInput("groups_2.1",label = "What grouping variable would you like to investigate?",
-                                                 data = COVID_Deaths,
+                                                 data = death,
                                                  selected = "Age group"),
                                   varSelectInput("groups_2.2",label = "If you would like to conduct a two-way ANOVA, please select another variable to investigate.",
-                                                 data = COVID_Deaths,
+                                                 data = death,
                                                  selected = "Not Applicable"),
                                   verbatimTextOutput("anova_2")),
                                   mainPanel(plotOutput("anovaPlot_2"))))
@@ -66,11 +66,11 @@ server <- function(input, output) {
    
   output$anova_2 <- renderPrint({ 
      if (!!input$type_2 == "One-way"){
-       two.way <- aov(Deaths ~ COVID_Deaths[[input$groups_2.1]], data = COVID_Deaths)
+       two.way <- aov(Deaths ~ death[[input$groups_2.1]], data = death)
        print (summary(two.way))
      }
      else if (!!input$type_2 == "Two-way"){
-       two.way <- aov(Deaths ~ COVID_Deaths[[input$groups_2.1]] + COVID_Deaths[[input$groups_2.2]], data = COVID_Deaths)
+       two.way <- aov(Deaths ~ death[[input$groups_2.1]] + death[[input$groups_2.2]], data = death)
        print (summary(two.way))
      }
   })
@@ -99,7 +99,7 @@ server <- function(input, output) {
  
  output$anovaPlot_2 <- renderPlot({
    if (!!input$type_2 == "One-way"){
-     COVID_Deaths%>%
+     death%>%
        group_by(!!input$groups_2.1)%>%
        ggplot(aes(!!input$groups_2.1, Deaths, fill = !!input$groups_2.1))+
        geom_boxplot(show.legend = FALSE)+ 
@@ -108,7 +108,7 @@ server <- function(input, output) {
        theme(axis.text.x = element_text(angle = 75, vjust = 0.5, hjust = 1))
      
    }else if (!!input$type_2 == "Two-way"){
-     COVID_Deaths%>%
+     death%>%
        group_by(!!input$groups_2.1, !!input$groups_2.2)%>%
        ggplot(aes(!!input$groups_2.1, Deaths, fill = !!input$groups_2.2))+
        geom_boxplot(show.legend = FALSE)+ 
